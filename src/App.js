82d7661fbc7a39cfef8e4e551fa3ce6e4bc87e64
David Miguel Yusta <36865163+neodmy@users.noreply.github.com>
2020-08-28 23:58:47 +0200

    Signin login (#2)
    * feat: add background color to main page
    
    * feat: add Navbar component
    
    * feat(wip): add form to sigin and login. Make requests to backend

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import AuthPage from "./pages/Auth";
 import BookingsPage from "./pages/Bookings";
 import EventsPage from "./pages/Events";
+import MainNavigation from "./components/Navigation/MainNavigation";
 
 import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect exact from="/" to="/auth" />
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={EventsPage} />
-        <Route path="/bookings" component={BookingsPage} />
-      </Switch>
+      <>
+        <MainNavigation />
+        <main className="main-content">
+          <Switch>
+            <Redirect exact from="/" to="/auth" />
+            <Route path="/auth" component={AuthPage} />
+            <Route path="/events" component={EventsPage} />
+            <Route path="/bookings" component={BookingsPage} />
+          </Switch>
+        </main>
+      </>
     </BrowserRouter>
   );
 };
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,7 +1,89 @@
-import React from "react";
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from "react";
+
+import "./Auth.css";
 
 const AuthPage = () => {
-  return <h1>The Auth Page</h1>;
+  const [isLogin, setIsLogin] = useState(true);
+
+  const emailEl = React.createRef();
+  const passwordEl = React.createRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const email = emailEl.current.value;
+    const password = passwordEl.current.value;
+
+    if (email.trim().label === 0 || password.trim().length === 0) return;
+
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `,
+    };
+
+    if (!isLogin) {
+      requestBody = {
+        query: `
+          mutation {
+            createUser(userInput: {email: "${email}", password: "${password}"}) {
+              _id
+              email
+            }
+          }
+        `,
+      };
+    }
+
+    fetch("http://localhost:4000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        console.log(resData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const switchModeHandler = () => {
+    setIsLogin(!isLogin);
+  };
+
+  return (
+    <form className="auth-form" onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="email">E-Mail</label>
+        <input type="email" id="email" ref={emailEl} />
+      </div>
+      <div className="form-control">
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" ref={passwordEl} />
+      </div>
+      <div className="form-actions">
+        <button type="submit">Submit</button>
+        <button type="button" onClick={switchModeHandler}>
+          Switch to {isLogin ? "Signup" : "Login"}
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default AuthPage;
